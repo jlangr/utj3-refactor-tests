f1a626f4e83601fdd9731797e626df6255376e68
Jeff L <jeff@langrsoft.com>
2024-03-26 16:27:55 -0600

    back to old school java

--- src/test/java/util/SearchTest.java
+++ src/test/java/util/SearchTest.java
@@ -4,49 +4,45 @@ package util;
 // http://www.gutenberg.org/cache/epub/2701/pg2701.txt 
 
 import org.junit.jupiter.api.Test;
+
 import java.io.*;
 import java.net.*;
+import java.util.*;
 import java.util.logging.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-// START:test
-// START_HIGHLIGHT
-class SearchTest {
-   // END_HIGHLIGHT
+public class SearchTest {
    @Test
-// START_HIGHLIGHT
-   void testSearch() {
-      // END_HIGHLIGHT
+   public void testSearch() {
       try {
-        var pageContent = "There are certain queer times and occasions "
-              + "in this strange mixed affair we call life when a man "
-              + "takes this whole universe for a vast practical joke, "
-              + "though the wit thereof he but dimly discerns, and more "
-              + "than suspects that the joke is at nobody's expense but "
-              + "his own.";
-         var bytes = pageContent.getBytes();
-         var stream = new ByteArrayInputStream(bytes);
-         //...
-         // END:test
+         String pageContent = "There are certain queer times and occasions "
+            + "in this strange mixed affair we call life when a man "
+            + "takes this whole universe for a vast practical joke, "
+            + "though the wit thereof he but dimly discerns, and more "
+            + "than suspects that the joke is at nobody's expense but "
+            + "his own.";
+         byte[] bytes = pageContent.getBytes();
+         ByteArrayInputStream stream = new ByteArrayInputStream(bytes);
          // search
-         var search = new Search(stream, "practical joke", "1");
+         Search search = new Search(stream, "practical joke", "1");
          Search.LOGGER.setLevel(Level.OFF);
          search.setSurroundingCharacterCount(10);
          search.execute();
          assertFalse(search.errored());
-         var matches = search.getMatches();
+         List<Match> matches = search.getMatches();
          assertNotNull(matches);
          assertTrue(matches.size() >= 1);
-         var match = matches.get(0);
+         Match match = matches.get(0);
          assertEquals("practical joke", match.searchString);
          assertEquals("or a vast practical joke, though t",
             match.surroundingContext);
          stream.close();
 
          // negative
-         var connection =
-               new URL("http://bit.ly/15sYPA7").openConnection();
-         var inputStream = connection.getInputStream();
+         URLConnection connection =
+            new URL("http://bit.ly/15sYPA7").openConnection();
+         InputStream inputStream = connection.getInputStream();
          search = new Search(
             inputStream, "smelt", "http://bit.ly/15sYPA7");
          search.execute();
@@ -57,6 +53,4 @@ class SearchTest {
          fail("exception thrown in test" + e.getMessage());
       }
    }
-   // START:test
 }
-// END:test
