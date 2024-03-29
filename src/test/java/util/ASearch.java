22c197d703d6b77508ccc66a18578bf7b6c9bd85
Jeff L <jeff@langrsoft.com>
2024-03-29 14:37:21 -0600

    remove io exception

--- src/test/java/util/ASearch.java
+++ src/test/java/util/ASearch.java
@@ -17,7 +17,7 @@ class ASearch {
    static final String A_TITLE = "1";
 
    @Test
-   void returnsMatchesWithSurroundingContext() throws IOException {
+   void returnsMatchesWithSurroundingContext() {
       var stream = streamOn("There are certain queer times and occasions "
           + "in this strange mixed affair we call life when a man "
           + "takes this whole universe for a vast practical joke, "
