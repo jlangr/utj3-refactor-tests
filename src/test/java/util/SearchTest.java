1ae20416dc410e953cb2c67051ddd5920c5b9a93
Jeff L <jeff@langrsoft.com>
2024-03-28 18:24:31 -0600

    part

--- src/test/java/util/SearchTest.java
+++ src/test/java/util/SearchTest.java
@@ -24,12 +24,12 @@ class SearchTest {
    void testSearch() throws IOException {
       // START_HIGHLIGHT
        var stream = streamOn("There are certain queer times and occasions "
+           // END_HIGHLIGHT
           + "in this strange mixed affair we call life when a man "
           + "takes this whole universe for a vast practical joke, "
           + "though the wit thereof he but dimly discerns, and more "
           + "than suspects that the joke is at nobody's expense but "
           + "his own.");
-       // END_HIGHLIGHT
       var search = new Search(stream, "practical joke", A_TITLE);
       // ...
       // END:test
@@ -63,4 +63,4 @@ class SearchTest {
    }
    // END_HIGHLIGHT
 }
-// STOP:test
+// END:test
