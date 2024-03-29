60d2d74c10492b896e416b9c444c1bfb43931a8d
Jeff L <jeff@langrsoft.com>
2024-03-28 19:04:41 -0600

    reflow text

--- src/test/java/util/SearchTest.java
+++ src/test/java/util/SearchTest.java
@@ -23,10 +23,10 @@ class SearchTest {
    void testSearch() throws IOException {
       // START:createStream
       var pageContent = "There are certain queer times and occasions "
-         + "in this strange mixed affair we call life when a man "
-         + "takes this whole universe for a vast practical joke, "
-         + "though the wit thereof he but dimly discerns, and more "
-         + "than suspects that the joke is at nobody's expense but his own.";
+          + "in this strange mixed affair we call life when a man takes "
+          + "this whole universe for a vast practical joke, though "
+          + "the wit thereof he but dimly discerns, and more than "
+          + "suspects that the joke is at nobody's expense but his own.";
       var bytes = pageContent.getBytes();
       var stream = new ByteArrayInputStream(bytes);
       // END:createStream
