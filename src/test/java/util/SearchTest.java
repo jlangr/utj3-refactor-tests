0b39e4e5edfcc19207137ae6d0847c42b6fedcb8
Jeff L <jeff@langrsoft.com>
2024-03-28 19:04:00 -0600

    reflow text

--- src/test/java/util/SearchTest.java
+++ src/test/java/util/SearchTest.java
@@ -20,10 +20,10 @@ class SearchTest {
       // ...
       // END:assertEmpty
       var pageContent = "There are certain queer times and occasions "
-         + "in this strange mixed affair we call life when a man "
-         + "takes this whole universe for a vast practical joke, "
-         + "though the wit thereof he but dimly discerns, and more "
-         + "than suspects that the joke is at nobody's expense but his own.";
+          + "in this strange mixed affair we call life when a man takes "
+          + "this whole universe for a vast practical joke, though "
+          + "the wit thereof he but dimly discerns, and more than "
+          + "suspects that the joke is at nobody's expense but his own.";
       var bytes = pageContent.getBytes();
       var stream = new ByteArrayInputStream(bytes);
       var search = new Search(stream, "practical joke", "1");
