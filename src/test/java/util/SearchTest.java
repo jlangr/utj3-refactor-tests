91aed7cac1bb64ece9ed49a4aa932321be7c1082
Jeff L <jeff@langrsoft.com>
2024-03-28 19:02:13 -0600

    reflow text

--- src/test/java/util/SearchTest.java
+++ src/test/java/util/SearchTest.java
@@ -18,10 +18,10 @@ public class SearchTest {
    public void testSearch() {
       try {
          String pageContent = "There are certain queer times and occasions "
-            + "in this strange mixed affair we call life when a man "
-            + "takes this whole universe for a vast practical joke, "
-            + "though the wit thereof he but dimly discerns, and more "
-            + "than suspects that the joke is at nobody's expense but his own.";
+            + "in this strange mixed affair we call life when a man takes "
+            + "this whole universe for a vast practical joke, though "
+            + "the wit thereof he but dimly discerns, and more than "
+            + "suspects that the joke is at nobody's expense but his own.";
          byte[] bytes = pageContent.getBytes();
          ByteArrayInputStream stream = new ByteArrayInputStream(bytes);
          // search
