af4ff85088241985db41101662869e63ca77061d
Jeff L <jeff@langrsoft.com>
2024-03-29 14:15:25 -0600

    AAA

--- src/test/java/util/ASearch.java
+++ src/test/java/util/ASearch.java
@@ -14,53 +14,55 @@ import java.util.logging.Level;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-// START:suppress
 class ASearch {
-   // ...
-   // END:suppress
    static final String A_TITLE = "1";
 
-   // START:suppress
    @BeforeEach
    void suppressLogging() {
       Search.LOGGER.setLevel(Level.OFF);
    }
-   // ...
-   // END:suppress
 
+   // START:test
    @Test
    void returnsMatchesWithSurroundingContext() throws IOException {
       var stream = streamOn("There are certain queer times and occasions "
+          // ...
+          // END:test
           + "in this strange mixed affair we call life when a man "
           + "takes this whole universe for a vast practical joke, "
           + "though the wit thereof he but dimly discerns, and more "
           + "than suspects that the joke is at nobody's expense but his own.");
-      // START:errored
+      // START:test
       var search = new Search(stream, "practical joke", A_TITLE);
       search.setSurroundingCharacterCount(10);
+      // START_HIGHLIGHT
+
+      // END_HIGHLIGHT
       search.execute();
       // START_HIGHLIGHT
-      assertFalse(search.errored());
+
       // END_HIGHLIGHT
       var matches = search.getMatches();
       assertEquals(List.of(
-          // ...
-              // END:errored
               new Match(A_TITLE,
                   "practical joke",
                   "or a vast practical joke, though t")),
           matches);
    }
 
-   // START:test
    @Test
    void returnsNoMatchesWhenSearchTextNotFound() throws IOException {
       var connection =
          new URL("http://bit.ly/15sYPA7").openConnection();
       try (var inputStream = connection.getInputStream()) {
          var search = new Search(inputStream, "smelt", A_TITLE);
+         // START_HIGHLIGHT
 
+         // END_HIGHLIGHT
          search.execute();
+         // START_HIGHLIGHT
+
+         // END_HIGHLIGHT
          assertTrue(search.getMatches().isEmpty());
       }
    }
